a6605d49a43752782520946b2a6cd1c632ef7f8b
yoseph ahmed <yosephahmed@aol.com>
2021-04-29 14:20:21 -0400

    feat: implemented returnPrimeNumbers method

--- app.js
+++ app.js
@@ -73,3 +73,29 @@ function germanNumbers(){
 
     return concatArr;
 }
+
+// source: https://urlzs.com/e8UJm
+function isPrime(n) {
+
+    // If n is less than 2 or not an integer then by definition cannot be prime.
+    if (n < 2) {return false}
+    if (n != Math.round(n)) {return false}
+ 
+    // Now assume that n is prime, we will try to prove that it is not.
+    var isPrime = true;
+ 
+    // Now check every whole number from 2 to the square root of n. If any of these divides n exactly, n cannot be prime.
+    for (var i = 2; i <= Math.sqrt(n); i++) {
+       if (n % i == 0) {isPrime = false}
+    }
+ 
+    // Finally return whether n is prime or not.
+    return isPrime;
+ 
+ }
+
+ // function that returns an array of ONLY prime numbers that are in the array numbers
+function returnPrimeNumbers(){
+    const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+    return numbers.filter(isPrime);
+}
